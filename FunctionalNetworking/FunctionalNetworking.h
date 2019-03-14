d96e956bb693f90794dc648dc4d5e55cc9c38026
Eduardo Sanches Bocato <dubocato@gmail.com>
2019-03-14 10:29:19 -0300

    Initial Commit

---
+++ FunctionalNetworking/FunctionalNetworking.h
@@ -0,0 +1,19 @@
+//
+//  FunctionalNetworking.h
+//  FunctionalNetworking
+//
+//  Created by Eduardo Bocato on 14/03/19.
+//  Copyright © 2019 Eduardo Bocato. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FunctionalNetworking.
+FOUNDATION_EXPORT double FunctionalNetworkingVersionNumber;
+
+//! Project version string for FunctionalNetworking.
+FOUNDATION_EXPORT const unsigned char FunctionalNetworkingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FunctionalNetworking/PublicHeader.h>
+
+
